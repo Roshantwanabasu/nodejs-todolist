c405e9c515ce47991d767bd93d3d2af96a096de6
Roshan Twanabasu <roshan.twanabasu@worldlink.com.np>
2022-07-12 13:18:18 +0545

    add mongo db connection

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require("express");
-
 const app = express();
+const dotenv = require("dotenv");
 
-app.use("/static", express.static("public"))
+dotenv.config();
 
-//view engine configurations
-app.set("view engine", "ejs")
-
-app.get("/", (req, res) => {
-    res.render("todo.ejs")
+const mongoose = require("mongoose");
+//connection to db
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
+  console.log("Connected to db!");
+  app.listen(3000, () => console.log("Server running at localhost:3000"));
 });
 
-app.listen(3000, () => console.log("Server running at localhost:3000"));
+app.use("/static", express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+//view engine configurations
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("todo.ejs");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+});
