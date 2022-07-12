111801fd4413e26137a14d947f8110d326567452
Roshan Twanabasu <roshan.twanabasu@worldlink.com.np>
2022-07-12 13:54:46 +0545

    add todo crud operations and add edit view and todo model

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const TodoTask = require("./models/TodoTask");
 
 dotenv.config();
 
-const mongoose = require("mongoose");
 //connection to db
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
   console.log("Connected to db!");
@@ -18,9 +19,46 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("todo.ejs");
+  TodoTask.find({}, (err, tasks) => {
+    res.render("todo.ejs", { todoTasks: tasks });
+  });
 });
 
-app.post("/", (req, res) => {
-  console.log(req.body);
+//POST METHOD
+app.post("/", async (req, res) => {
+  const todoTask = new TodoTask({
+    content: req.body.content,
+  });
+  try {
+    await todoTask.save();
+    console.log("Added TodoTask Success");
+    res.redirect("/");
+  } catch (err) {
+    console.log("Create Todo Error:".err);
+    res.redirect("/");
+  }
+});
+
+app
+  .route("/edit/:id")
+  .get((req, res) => {
+    const id = req.params.id;
+    TodoTask.find({}, (err, tasks) => {
+      res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id });
+    });
+  })
+  .post((req, res) => {
+    const id = req.params.id;
+    TodoTask.findByIdAndUpdate(id, { content: req.body.content }, (err) => {
+      if (err) return res.send(500, err);
+      res.redirect("/");
+    });
+  });
+
+app.route("/remove/:id").get((req, res) => {
+  const id = req.params.id;
+  TodoTask.findByIdAndRemove(id, (err) => {
+    if (err) return res.send(500, err);
+    res.redirect("/");
+  });
 });
